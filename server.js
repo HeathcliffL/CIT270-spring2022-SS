b445ef18385d7f92faa7bf120bdb8215a57de8af
HeathcliffL <49813952+HeathcliffL@users.noreply.github.com>
2022-05-13 14:05:10 -0600

    import redis libray

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 //import express library
 const express = require('express');
 
+//set the port to
+const port = 3000;
+
 //body paerser is called middleware
 const bodyParser = require('body-parser');
 const { request } = require('express');
 const { response } = require('express');
 
 //import MD5
-const md5 = require('md5')
+const md5 = require('md5');
 
-//set the port to
-const port = 3000;
+//require the node-redis libary
+const redis = require('redis');
+//use this to connocting redis database
+const client = redis.createClient(port,);
 
 //use the library
 const app = express();
@@ -27,18 +32,21 @@ app.listen(port, ()=>{
 app.get('/', (request,response)=>{res.send("Hello")});
 
 //a post is when a client sends new information to an API
-app.post('/login',(request,response)=>{
+app.post('/login',async (request,response)=>{
     const loginRequset = request.body;
     console.log("requset Body",JSON.stringify(request.body))
+
     //serch database for username, and retrieve current password
+    const hashedPassword = md5(request.body.password);
+    const redisPassword = await client.hGet('user', request.body.userName);
 
     //compare the hashed version password that was sent with the hashed version from the database
     
-    if (loginRequset.userName == "357@753.com" && loginRequset.password == "Qw123$"){
+    /*if (loginRequset.userName == "357@753.com" && loginRequset.password == "Qw123$"){
         response.status(200);//200 means OK
         response.send("Welcome");
     }else{
         response.status(401);//401 means unauthorized
         response.send("unauthorized");
-    }
+    }*/
 });
