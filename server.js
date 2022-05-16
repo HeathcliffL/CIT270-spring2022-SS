0be6b2ed3efb043a182ded1b732b818c656b6028
HeathcliffL <49813952+HeathcliffL@users.noreply.github.com>
2022-05-16 13:47:17 -0600

    fix the Bug about using the redis

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ const { response } = require('express');
 //import MD5
 const md5 = require('md5');
 
-//require the node-redis libary
-const redis = require('redis');
+//require the redis libary
+const {createClient} = require('redis');
 //use this to connocting redis database
-const client = redis.createClient(port,);
+const client = createClient();
+client.connect();
 
 //use the library
 const app = express();
@@ -32,17 +33,27 @@ app.listen(port, ()=>{
 app.get('/', (request,response)=>{res.send("Hello")});
 
 //a post is when a client sends new information to an API
-app.post('/login',async (request,response)=>{
-    const loginRequset = request.body;
-    console.log("requset Body",JSON.stringify(request.body))
+app.post('/login',  async (request,response)=>{
 
     //serch database for username, and retrieve current password
     const hashedPassword = md5(request.body.password);
     const redisPassword = await client.hGet('user', request.body.userName);
+    const loginRequset = request.body;
 
     //compare the hashed version password that was sent with the hashed version from the database
     
-    /*if (loginRequset.userName == "357@753.com" && loginRequset.password == "Qw123$"){
+    if (loginRequset.userName == "357@753.com" && hashedPassword == redisPassword){
+        response.status(200);//200 means OK
+        response.send("Welcome");
+    }else{
+        response.status(401);//401 means unauthorized
+        response.send("unauthorized");
+    }
+    /*
+    const loginRequset = request.body;
+    console.log("requset Body",JSON.stringify(request.body))
+
+    if (loginRequset.userName == "357@753.com" && loginRequset.password == "Qw123$"){
         response.status(200);//200 means OK
         response.send("Welcome");
     }else{
@@ -50,3 +61,8 @@ app.post('/login',async (request,response)=>{
         response.send("unauthorized");
     }*/
 });
+
+/*Useing this you need change the line 36 
+app.post('/login',  async (request,response) to const validatePassword = async (request,response)..
+*/
+//app.post('/login', validatePassword);  
